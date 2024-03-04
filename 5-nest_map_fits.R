8c7cb85c273a4b307e87c85763e892bcf0110cef
colinbousige <colin.bousige@gmail.com>
2024-03-04 12:18:08 +0100

    added fits

--- 5-nest_map_fits.R
+++ 5-nest_map_fits.R
@@ -14,41 +14,45 @@ theme_set(theme_bw()+
           strip.text = element_text(face = "bold"),
           strip.text.y = element_text(angle=0)))
 
-# Find list of all files in the Data directory with the pattern "sample" in their name
-flist <- list.files(___
-                    full.names = TRUE)
+# Find list of all files in the Data/fits directory with the pattern "csv" in their name
+flist <- list.files(___)
 # Read the files in the list and store them in a list of dataframes
-d <- read_csv(___, id='file') |>
-    nest(___)  |> # nest the data in a list column called "data"
+d <- read_csv(___) |>
+    nest(data = ___)  |> # nest the data in a list column called "data"
     mutate(___) |>  # remove the path from the file name
-    separate(___) |>  # retrieve the information from the file name and store it into new columns called "sample", "temperature", and "time"
-    mutate(___, # remove the string "sample" and convert to factor
-           ___, # remove the string "K" and convert to numeric
-           ___, # remove all digits from the string "time" and store the result in a column "time_unit"
-           ___, # remove all letters from the string "time" and convert to numeric
-           ___) |> # convert the time to seconds if the time unit is "min"
-    select(___)  # remove the column "time_unit"
+    separate(___) |>  # retrieve the information from the file name and store it into new columns
+    mutate(temperature = ___, # remove the string "K" and convert to numeric
+           time = ___, # remove all letters from the string "time" and convert to numeric
+           ) |> 
+    mutate(Lm = map_dbl(data, ___), # using map_dbl(), compute the mean of the column "L" for each dataframe in the list column "data"
+           Ls = ___) # using map_dbl(), compute the standard deviation of the column "L" for each dataframe in the list column "data"
 
 # Write a function to fit a linear model to the data given a dataframe "df"
-# with columns "x" and "y"
+# with columns "Lm" and "time" and use "1/Ls^2" for the weights
 myfit <- function(df){
-    ___
+    lm(___)
 }
 
 # Fit the linear model to each dataframe in the list column "data"
 # using the functions "myfit" and "map()"
 d_fitted <- d |>
-    mutate(fit = map(___), # do the fit on all elements (tibbles) of the column "data"
-           tidied = map(___), # tidy the results of the fit
-           augmented = map(___) # augment the results of the fit
+    ___ |> # remove the column "data" (we don't need it anymore
+    nest(___) |> # nest the data as a function of temperature in a list column called "data"
+    mutate(fit = ___, # do the fit on all elements (tibbles) of the column "data"
+           tidied = ___, # tidy the results of the fit
+           augmented = ___ # augment the results of the fit
            )
 
+
 # Plot the evolution of the parameters of the fits
+# Add vertical error bars
 d_fitted |>
     unnest(tidied) |>
-    ___
+    mutate(term = ifelse(term == "(Intercept)", "Intercept", "Slope")) |> # rename the term "(Intercept)" to "intercept" and the term "x" to "slope"
+    ggplot(___)
 
 # Plot the data and the fits
 d_fitted |>
     unnest(augmented) |>
-    ___
+    ggplot(___)
+
